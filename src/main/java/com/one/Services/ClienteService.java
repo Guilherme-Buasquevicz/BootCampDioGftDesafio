b5923bc5965b5f8fa38ea54f391be9440223b535
Guilherme-Buasquevicz <buasquevicz22@outlook.com>
2022-01-27 18:11:57 -0300

    Projeto Spring

---
+++ src/main/java/com/one/Services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.one.Services;
+
+import com.one.Model.Cliente;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ *
+ * @author Guilherme-Buasquevicz
+ */
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
